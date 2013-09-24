24ed558447590d7f6c059d3a3057226283ca9cdb
gaoguqi <gaotao3037@gmail.com>
2013-09-24 22:49:49 +0800

    2013092402

--- scripts/global.js
+++ scripts/global.js
@@ -201,7 +201,7 @@ function preparePlaceholder() {
   if (!document.getElementById("imagegallery")) return false;
   var placeholder = document.createElement("img");
   placeholder.setAttribute("id","placeholder");
-  placeholder.setAttribute("src","images/placeholder.gif");
+  placeholder.setAttribute("src","/images/placeholder.gif");
   placeholder.setAttribute("alt","my image gallery");
   var description = document.createElement("p");
   description.setAttribute("id","description");
