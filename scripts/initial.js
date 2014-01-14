04c695d042d9afd931cf98d4bd498d48aaede2a4
gaoguqi <gaotao3037@gmail.com>
2014-01-14 11:28:50 +0800

    2014011321

---
+++ scripts/initial.js
@@ -0,0 +1,11 @@
+$(function(){
+		$("tbody>tr:odd").addClass("odd"); //先排除第一行,然后给奇数行添加样式
+		$("tbody>tr:even").addClass("even"); //先排除第一行,然后给偶数行添加样式
+		$("tr:contains('周日')").addClass("selected");
+		var footer = $("<div id='footer'>All copyrights reserved by GaoGuqi.</div>");
+		$("body").append(footer);
+		
+		var header = $("<p class='siteName'><a href='http://www.gaoguqi.com' id='logo' title='TG AND LR'S BLOG'>高古奇的学习探索</a></p><p class='description'>增长经验，弥补不足，纠正习惯</p>");
+		$("body header").prepend(header);
+		
+  })
